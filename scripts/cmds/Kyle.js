5928ce93d03185c370464ec52cbf6d3d75651b10
Yanzusitedrak <164723620+Yanzusitedrak@users.noreply.github.com>
2024-04-09 22:03:53 +0800

    Update and rename Jay.js to Kyle.js

--- scripts/cmds/Kyle.js
+++ scripts/cmds/Kyle.js
@@ -1,8 +1,8 @@
 module.exports = {
     config: {
-        name: "jay",
+        name: "Kyle",
         version: "1.0",
-        author: "Jay Senpai",
+        author: "Kyle Senpai",
         countDown: 5,
         role: 0,
         shortDescription: "sarcasm",
@@ -16,10 +16,10 @@ module.exports = {
         getLang
     }) {
         const lowerCaseBody = event.body.toLowerCase();
-        if (lowerCaseBody.includes("jay") || lowerCaseBody.includes("jay d bohol") ||
-lowerCaseBody.includes("bohol") ||
-lowerCaseBody.includes("Jay D Bohol")) {
-            return message.reply(`busy pa master ko`);
+        if (lowerCaseBody.includes("kyle") || lowerCaseBody.includes("kyle Bait-it") ||
+lowerCaseBody.includes("Kyle") ||
+lowerCaseBody.includes("@Kyle Bait-it")) {
+            return message.reply(`kaylangan mo sa master ko?`);
         }
     }
 };
